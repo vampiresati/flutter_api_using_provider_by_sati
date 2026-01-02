29b31d3dee36538e598b8f16b9775d438c882539
Satvir <sssatvir12@gmail.com>
2026-01-02 21:41:46 +0530

    adding

---
+++ first_app_by_sati/lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Provider api example')),
+      body: const Center(child: Text('Provider api example!')),
+    );
+  }
+}
--- first_app_by_sati/lib/main.dart
+++ first_app_by_sati/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app_by_sati/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,14 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello, Flutter!'),
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
     );
   }
 }
